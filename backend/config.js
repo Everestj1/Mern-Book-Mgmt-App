72e998837b0b802f74713e5c266b21dcbb70bf95
Ever888 <everestjawed1@gmail.com>
2025-08-30 11:30:02 +0500

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://ever888:Mongodb.1@mern-bookstore.t8rhyeh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=mern-bookstore";
---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for parsing request body from different domain,protocols, or ports
+ // option 1
+app.use(cors());
+// option 2
+// app.use(cors({
+//     origin: "https://localhost:3000",
+//     methods: ["GET","POST","PUT","DELETE"],
+//     allowedHeaders: ["Content-Type"]
+// }));
+
+// Router for Book Routes
+app.use("/books", booksRoute);
+
+
+// app.get("/", (req, res) => {
+//      console.log(req);
+//      return res.status(234).send("Welcome to MERN Stack.");
+// });
+
+
+mongoose.connect(mongoDBURL)
+.then(()=>{
+    console.log("App connected to DB");
+    app.listen(PORT, ()=>{
+        console.log(`App is listening on port : ${PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log(error);
+});
